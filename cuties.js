b5baf2f042724b20fff750ce14e5707ccac8c10d
tghil7 <tghil7@users.noreply.github.com>
2017-04-01 10:01:43 -0500

    Add files via upload

--- cuties.js
+++ cuties.js
@@ -1,3 +1,12 @@
 /**
  * Created by Anicet on 9/19/2015.
  */
+
+ function activate (){
+	 $(document).ready(function(){
+		$("li").removeClass('active');
+		$(this).parent().addClass('active');
+         	
+		
+	 });
+ }
